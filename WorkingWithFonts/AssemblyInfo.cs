4f9fca004f110ffabec766e39928ba2ee7ea1039
gwise <gwise@GWISE-PC>
2020-12-19 14:09:49 +0900

    Add project files.

---
+++ WorkingWithFonts/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Lobster-Regular.ttf", Alias = "Lobster")]
+[assembly: ExportFont("digital-7.ttf", Alias = "Digital-7")]
